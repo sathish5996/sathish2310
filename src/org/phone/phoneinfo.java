f160ad3b7c95300631f87ae05d9f6700f99deed5
sathish5996 <sri.sathish1996@gmail.com>
2022-09-03 13:18:14 +0530

    kumar

---
+++ src/org/phone/phoneinfo.java
@@ -0,0 +1,40 @@
+package org.phone;
+
+public class phoneinfo {
+
+	private void phoneName() {
+		System.out.println("mi");
+
+	}
+
+	private void phoneMieiNum() {
+		System.out.println("123456789");
+
+	}
+
+	private void phoneNmber() {
+		System.out.println("9444167397");
+
+	}
+
+	private void camera() {
+		System.out.println("20mgph");
+
+	}
+
+	private void storage() {
+		System.out.println("8gb");
+
+	}
+
+	public static void main(String[] args) {
+		phoneinfo a = new phoneinfo();
+		a.phoneName();
+		a.phoneMieiNum();
+		a.phoneNmber();
+		a.camera();
+		a.storage();
+
+	}
+
+}
